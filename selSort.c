9af49b9a47116ce82c3e7d1a8251f384d74d4611
Swati A Firangi <83855603+SwatiAF@users.noreply.github.com>
2022-06-15 10:29:12 +0530

    selectio sort

--- selSort.c
+++ selSort.c
@@ -1,74 +1,87 @@
 #include<stdio.h>
-#include<time.h>
 #include<stdlib.h>
+#include<time.h>
 
-void selectionSort(int n, int a[]);
+void selsort(int n, int a[]);
 
-void main() {
-int a[6] = {34, 67, 12, 5, 38, 90}, n, i, j, ch, temp;
-clock_t start, end;
+int main()
+{
+    int a[15000],n,i,j,ch,temp;
+    clock_t start,end;
+    while(1)
+{
+    printf("\n 1:For manual entry of N value and array elements ");
+    printf("\n 2:To display time taken for sorting number of elements N in the range 500 to 14500");
+    printf("\n 3:To exit ");
+    printf("\n Enter your choice:");
+    scanf("%d", &ch);
 
-while(1) {
-printf("\n1.Enter values\n2.Time display\n3.Exit\n");
-printf("\nEnter your choice: ");
-scanf("%d", &ch);
 
-switch(ch) {
-    case 1: //printf("Enter the number of elements: ");
-            
-            //scanf("%d", &n);
-            printf("Enter array elements: ");
-            for(i = 0; i <n; i++) {
-                scanf("%d", a[i]);
-            }
-            start = clock();
-            selectionSort(n, a);
-            end = clock();
-            printf("\nSorted array: ");
-            for(i = 0; i < n; i++)
-            printf("%d\t", a[i]);
-            printf("\nTime taken to sort %d numbers is %f seconds", n, (((double)(end-start))/CLOCKS_PER_SEC));
-            break;
-
-    case 2: n = 6;
-    while(n < 6) {
-        for(i = 0; i < 6; i++) {
-            //a[i] = rand();
-            a[i] = 6 - i;
-        }
-        start = clock();
-        selectionSort(n, a);
-
-        for(j = 0; j < 500000; j++) {
-            temp = 38/600;
-        }
-        end = clock();
-        printf("Time taken to sort %d numbers id %f sec", n, (((double)(end - start))/CLOCKS_PER_SEC));
-//n = n + 100;
+switch(ch)
+{
+    case 1: printf("\nEnter the number of elements: ");
+    scanf("%d",&n);
+    printf("\nEnter array elements: ");
+    for(i=0;i<n;i++)
+    {
+    scanf("%d",&a[i]);
     }
+    start=clock();
+    selsort(n,a);
+    end=clock();
+
+    printf("\nSorted array is: ");
+    for(i=0;i<n;i++)
+    printf("%d\t",a[i]);
+
+    printf("\n Time taken to sort %d numbers is %f Secs",n, (((double)(end-start))/CLOCKS_PER_SEC));
     break;
 
-    case 3: exit(0);
-    }
+    case 2:
+        n=500;
+        while(n<=14500)
+         {
+            for(i=0;i<n;i++)
+{
+a[i]=n-i;
+}
+start=clock();
+selsort(n,a);
 
-        getchar();
-    }
-    }
+//Dummy loop to create delay
+for(j=0;j<500000;j++)
+    { temp=38/600;}
+end=clock();
 
+printf("\n Time taken to sort %d numbers is %f Secs ",n, (((double)(end-start))/CLOCKS_PER_SEC));
+n=n+1000;
+}
+break;
 
-void selectionSort(int n, int a[]) {
-   int i, j, t, small, pos;
-   for(i = 0; i < n-1; j++) {
-    pos = i;
-    small = a[i];
-    for(j = t+1; j < n; j++) {
-        if(a[j] < small) {
-            small = a[j];
-            pos = j;
-        }
-    }
-    t = a[i];
-    a[i] = a[pos];
-    a[pos] = t;
-   }
+case 3: exit(0);
+}
+getchar();
+}
+return 0;
+}
+
+void selsort(int n,int a[])
+{
+int i,j,t,small,pos;
+for(i=0;i< n-1;i++)
+{
+pos=i;
+small=a[i];
+for(j=i+1;j<n;j++)
+{
+if(a[j]<small)
+{
+small=a[j];
+pos=j;
+}
+}
+t=a[i];
+a[i]=a[pos];
+a[pos]=t;
+}
 }
