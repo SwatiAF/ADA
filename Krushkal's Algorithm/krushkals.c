808212db3937d01cf975a1432943aabb6fa5bd09
Swati A Firangi <83855603+SwatiAF@users.noreply.github.com>
2022-08-10 10:15:28 +0530

    add program

--- Krushkal's Algorithm/krushkals.c
+++ Krushkal's Algorithm/krushkals.c
@@ -1,66 +1,55 @@
-#include&lt;stdio.h&gt;
-#include&lt;conio.h&gt;
+#include<stdio.h>
+
 void kruskals();
-int c[10][10],n;
-void main()
-{
-int i,j;
-clrscr();
-printf(&quot;\nenter the no. of vertices:\t&quot;);
-scanf(&quot;%d&quot;,&amp;n);
-printf(&quot;\nenter the cost matrix:\n&quot;);
-for(i=1;i&lt;=n;i++)
-{
-for(j=1;j&lt;=n;j++)
-{
-scanf(&quot;%d&quot;,&amp;c[i][j]);
-}
-}
-kruskals();
-getch();
+int c[10][10], n;
+
+void main() {
+    int i, j;
+
+    printf("\nEnter the num of vertices: \t");
+    scanf("%d", &n);
+    printf("\nEnter the cost matrix: \n");
+    for(i = 1; i <= n; i++) {
+    for(j = 1; j <= n; j++) {
+        scanf("%d", &c[i][j]);
+    }
+    }
+    kruskals();
+
 }
 
-void kruskals()
-{
-int i,j,u,v,a,b,min;
-int ne=0,mincost=0;
-int parent[10];
-for(i=1;i&lt;=n;i++)
-{
-parent[i]=0;
-}
-while(ne!=n-1)
-{
-min=9999;
-for(i=1;i&lt;=n;i++)
-{
-for(j=1;j&lt;=n;j++)
-{
-if(c[i][j]&lt;min)
-{
-min=c[i][j];
-u=a=i;
-v=b=j;
-}
-}
-}
-  
-while(parent[u]!=0)
-{
-u=parent[u];
-}
-while(parent[v]!=0)
-{
-v=parent[v];
-}
-if(u!=v)
-{
-printf(&quot;\n%d-----&gt;%d=%d\n&quot;,a,b,min);
-parent[v]=u;
-ne=ne+1;
-mincost=mincost+min;
-}
-c[a][b]=c[b][a]=9999;
-}
-printf(&quot;\nmincost=%d&quot;,mincost);
+void kruskals() {
+    int i, j, u, v, a, b, min;
+    int ne = 0, mincost = 0;
+    int parent[10];
+    for(i = 1; i <= n; i++) {
+        parent[i] = 0;
+    }
+    while(ne != n - 1) {
+        min = 9999;
+        for(i = 1; i <= n; i++) {
+        for(j = 1; j <= n; j++) {
+            if(c[i][j] < min) {
+                min = c[i][j];
+                u = a = i;
+                v = b = j;
+            }
+        }
+        }
+
+        while(parent[u] != 0) {
+            u = parent[u];
+        }
+        while(parent[v] != 0) {
+            v = parent[v];
+        }
+        if(u != v) {
+            printf("\n%d----->%d = %d\n", a, b, min);
+            parent[v] = u;
+            ne = ne + 1;
+            mincost = mincost + min;
+        }
+        c[a][b] = c[b][a] = 9999;
+    }
+    printf("\nmincost = %d", mincost);
 }
