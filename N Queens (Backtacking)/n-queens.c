ae7ddc08eb7c27e76ea22b15719959b386b82762
Swati A Firangi <83855603+SwatiAF@users.noreply.github.com>
2022-08-14 23:27:16 +0530

    add indentation

--- N Queens (Backtacking)/n-queens.c
+++ N Queens (Backtacking)/n-queens.c
@@ -7,7 +7,7 @@ void nqueens(int n) {
   x[k] = 0;
   while(k != 0) {
     x[k]++;
-    while(place(x,k) != 1 && x[k] <= n)
+    while(place(x, k) != 1 && x[k] <= n)
     x[k]++;
     if(x[k] <= n) {
       if(k == n) {
@@ -27,7 +27,7 @@ void nqueens(int n) {
 int place(int x[], int k) {
   int i;
   for(i = 1; i <= k - 1; i++) {
-    if(i + x[i] == k + x[k]||i - x[i]==k - x[k]||x[i] == x[k])
+    if(i + x[i] == k + x[k]||i - x[i] == k - x[k]||x[i] == x[k])
     return 0;
   }
   return 1;
